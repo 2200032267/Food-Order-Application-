67aaf03faa8b0869572cfcb5383f1b2c39e10a0a
2200032267 <2200032267@kluniversity.in>
2026-04-28 22:03:16 +0530

    Added backend (Spring Boot)

---
+++ src/main/java/com/vighnesh/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.vighnesh.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "MySuperSecretKeyForJWTThatIsLongEnough12345";
+    public static final String JWT_HEADER = "Authorization";
+
+}
